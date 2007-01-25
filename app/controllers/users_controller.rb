3ea000ff33745f9fdd8a3374fb64e2b95b362f25
Jeremy D. Frens <jdfrens@gmail.com>
2007-01-25 02:34:21 +0000

    slight change based on changes to LWT Authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   acts_as_login_controller
 
-  redirect_after_login do |controller, user|
+  redirect_after_login do |controller|
     { :controller => "home", :action => 'administrate' }
   end
 
