f6212d7f212b3e590176385de51c255f8165a495
Jeremy D. Frens <jdfrens@gmail.com>
2007-05-16 19:09:19 +0000

    Cleaned up the code a bit (mostly changing "should"s to "def test_"s).

--- test/unit/news_item_test.rb
+++ test/unit/news_item_test.rb
@@ -4,88 +4,88 @@ class NewsItemTest < Test::Unit::TestCase
   
   fixtures :news_items, :users
   
-  should "have connection to creators" do
+  def test_have_connection_to_creators
     assert_equal User.find(1), news_items(:todays_news).user
     assert_equal User.find(2), news_items(:another_todays_news).user
   end
   
-  should "not create news item without headline" do
+  def test_headline_required
     news_item = NewsItem.new(
-                             :headline => '',
-    :teaser => 'a teaser', :content => 'Valid content.',
-    :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago, 
-    :user_id => 1
-    )
+        :headline => '',
+        :teaser => 'a teaser', :content => 'Valid content.',
+        :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago, 
+        :user_id => 1
+        )
     assert !news_item.valid?
     assert_equal "can't be blank", news_item.errors[:headline]
   end
   
-  should "not create news item without a teaser" do
+  def test_teaser_required
     news_item = NewsItem.new(
-                             :headline => 'Valid headline',
-    :teaser => '', :content => 'Valid content.',
-    :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago,
-    :user_id => 1
-    )
+        :headline => 'Valid headline',
+        :teaser => '', :content => 'Valid content.',
+        :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago,
+        :user_id => 1
+        )
     assert !news_item.valid?
     assert_equal "can't be blank", news_item.errors[:teaser]
   end
   
-  should "not create news item without content" do
+  def test_content_required
     news_item = NewsItem.new(
-                             :headline => 'Valid headline',
-    :teaser => 'a teaser', :content => '',
-    :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago,
-    :user_id => 1
-    )
+        :headline => 'Valid headline',
+        :teaser => 'a teaser', :content => '',
+        :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago,
+        :user_id => 1
+        )
     assert !news_item.valid?
     assert_equal "can't be blank", news_item.errors[:content]
   end
   
-  should "not create news item without expires-at date" do
+  def test_expires_at_required
     news_item = NewsItem.new(
-                             :headline => 'Valid headline',
-    :teaser => 'a teaser', :content => 'Valid content.',
-    :goes_live_at => 1.day.ago,
-    :user_id => 1
-    )
+        :headline => 'Valid headline',
+        :teaser => 'a teaser', :content => 'Valid content.',
+        :goes_live_at => 1.day.ago,
+        :user_id => 1
+        )
     assert !news_item.valid?
     assert_equal "can't be blank", news_item.errors[:expires_at]
   end
   
-  should "not create news item without goes-live date" do
+  def test_goes_live_at_required
     news_item = NewsItem.new(
-                             :headline => 'Valid headline',
-    :teaser => 'a teaser', :content => 'Valid content.',
-    :expires_at => 1.hour.ago,
-    :user_id => 1
-    )
+        :headline => 'Valid headline',
+        :teaser => 'a teaser', :content => 'Valid content.',
+        :expires_at => 1.hour.ago,
+        :user_id => 1
+        )
     assert !news_item.valid?
     assert_equal "can't be blank", news_item.errors[:goes_live_at]
   end
   
-  should "not create news item without user" do
+  def test_user_required
     news_item = NewsItem.new(
-                             :headline => 'Valid headline',
-    :teaser => 'a teaser', :content => 'Valid content.',
-    :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago
-    )
+        :headline => 'Valid headline',
+        :teaser => 'a teaser', :content => 'Valid content.',
+        :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago
+        )
     assert !news_item.valid?
     assert_equal "is invalid", news_item.errors[:user]
   end
   
-  should "not create news item with invalid user" do
+  def test_valid_user_required
     news_item = NewsItem.new(
-                             :headline => 'Valid headline',
-    :teaser => 'a teaser', :content => 'Valid content.',
-    :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago,
-    :user_id => 99
-    )
+        :headline => 'Valid headline',
+        :teaser => 'a teaser', :content => 'Valid content.',
+        :goes_live_at => 1.day.ago, :expires_at => 1.hour.ago,
+        :user_id => 99
+        )
     assert !news_item.valid?
     assert_equal "is invalid", news_item.errors[:user]
   end
   
-  should "find just current news" do
+  def test_find_just_current_news
     news_items = NewsItem.find_current
     assert_equal 2, news_items.size
     assert news_items.include?(news_items(:todays_news))
@@ -103,7 +103,7 @@ class NewsItemTest < Test::Unit::TestCase
     assert news_items.include?(news_items(:past_news))
   end
   
-  should "find all through filter" do
+  def test_find_filtered_all
     found_items = NewsItem.find_filtered_news('all')
     assert_equal 4, found_items.size
     assert found_items.include?(news_items(:todays_news))
@@ -112,14 +112,14 @@ class NewsItemTest < Test::Unit::TestCase
     assert found_items.include?(news_items(:future_news))
   end
   
-  should "find current through filter" do
+  def test_find_filtered_current
     found_items = NewsItem.find_filtered_news("current")
     assert_equal 2, found_items.size
     assert found_items.include?(news_items(:todays_news))
     assert found_items.include?(news_items(:another_todays_news))
   end
   
-  should "know if current" do
+  def test_is_current
     assert news_items(:todays_news).is_current?
     assert news_items(:another_todays_news).is_current?
     assert !news_items(:past_news).is_current?
@@ -159,13 +159,14 @@ class NewsItemTest < Test::Unit::TestCase
     assert_equal Time.parse('01/05/1993'), item.goes_live_at
   end
   
-  should "render textile using RedCloth" do
+  def test_rendered_content
+    assert_equal "Something happened *today*.", news_items(:todays_news).content
     assert_equal "Something happened <strong>today</strong>.",
-    news_items(:todays_news).render_content
+        news_items(:todays_news).render_content
     assert_equal "Something else happened today.",
-    news_items(:another_todays_news).render_content
+        news_items(:another_todays_news).render_content
     assert_equal "Something happened in the distant <em>past</em>.",
-    news_items(:past_news).render_content
+        news_items(:past_news).render_content
   end
   
   #
