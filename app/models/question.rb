2fe119da0733ae5e211a1fb8f56438299808cb86
Jeremy D. Frens <jdfrens@gmail.com>
2007-08-14 18:08:17 +0000

    Question acts_as_list for Faq.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
 
   belongs_to :faq
+  acts_as_list :scope => :faq
   
   validates_presence_of [:query, :answer, :faq_id]
   
--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -11,4 +11,19 @@ class QuestionTest < Test::Unit::TestCase
     assert_equal faqs(:student_faq), questions(:student1).faq
     assert_equal faqs(:student_faq), questions(:student2).faq
   end
+
+  def test_acts_as_list_for_faq
+    faq = faqs(:student_faq)
+    question1 = questions(:student1)
+    question2 = questions(:student2)
+    assert_equal [question1, question2], faq.questions
+    
+    faq.questions[0].move_lower
+    faq.reload
+    question1.reload
+    question2.reload
+    
+    assert_equal [question2, question1], faq.questions(true)
+  end
+
 end
