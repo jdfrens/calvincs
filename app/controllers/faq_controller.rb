ba4028b1cff418695b5ed10a4c09a2a9b9ff7d09
Jeremy D. Frens <jdfrens@gmail.com>
2007-08-14 20:49:39 +0000

    FAQs can be created.

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,5 +1,7 @@
 class FaqController < ApplicationController
   
+  restrict_to :edit, :except => [ :index, :list, ]
+
   def index
     redirect_to :action => :list
   end
@@ -7,5 +9,17 @@ class FaqController < ApplicationController
   def list
     render :nothing => true
   end
-  
+
+  def create
+    if request.post?
+      @faq = Faq.create(params[:faq])
+      if @faq.valid?
+        redirect_to :action => :view, :id => @faq.identifier
+      else
+        render
+      end
+    else
+      render
+    end
+  end
 end
--- test/functional/faq_controller_test.rb
+++ test/functional/faq_controller_test.rb
@@ -7,6 +7,7 @@ class FaqController; def rescue_action(e) raise e end; end
 class FaqControllerTest < Test::Unit::TestCase
   
   fixtures :faqs, :questions
+  user_fixtures
   
   def setup
     @controller = FaqController.new
@@ -17,5 +18,53 @@ class FaqControllerTest < Test::Unit::TestCase
   def test_index_redirects_to_list
     get :index
     assert_redirected_to :action => :list
+  end
+  
+  def test_create_faq
+    get :create, {}, user_session(:edit)
+    
+    assert_response :success
+    assert_standard_layout
+    
+    assert_select "form[action=/faq/create]" do
+      assert_select "input#faq_title", true
+      assert_select "input#faq_identifier", true
+      assert_select "input[type=submit]", true
+    end
+  end
+  
+  def test_create_faq_redirects_when_NOT_logged_in
+    get :create
+    assert_redirected_to_login
+  end
+  
+  def test_create_faq_creates_faq
+    post :create,
+        { :faq => {
+          :title => "FAQ Title",
+          :identifier => "faq_identifier"} },
+        user_session(:edit)
+        
+    assert_redirected_to :action => :view, :id => "faq_identifier"
+    
+    faq = Faq.find_by_identifier("faq_identifier")
+    assert_not_nil faq
+    assert_equal "FAQ Title", faq.title
+  end
+  
+  def test_create_faq_fails_on_bad_data
+    post :create,
+        { :faq => {
+          :title => "",
+          :identifier => "faq identifier"} },
+        user_session(:edit)
+        
+    assert_response :success
+    assert_standard_layout
+    assert_select "div#errorExplanation" do
+      assert_select "li", /title/i, "title should be bad"
+      assert_select "li", /identifier/i, "identifier should be bad"
+    end
+    assert_select "form[action=/faq/create]"
   end
 end
