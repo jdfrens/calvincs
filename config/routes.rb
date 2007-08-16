0bd5ae5b322f40798d7347e55e56d4218eded942
Jeremy D. Frens <jdfrens@gmail.com>
2007-08-16 14:20:29 +0000

    Bleepidy-bleep Subclipse!

--- config/routes.rb
+++ config/routes.rb
@@ -11,14 +11,14 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "home"
+  map.home '', :controller => "home"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
   map.connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Special Calvin CS routes
-  map.connect 'p/:id', :controller => 'page', :action => 'view', :id => /\w+/
+  map.view_page 'p/:id', :controller => 'page', :action => 'view', :id => /\w+/
   
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
