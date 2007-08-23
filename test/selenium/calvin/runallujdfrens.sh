791dfc91392a59ec82bcdbecd288983f9a28ae4a
Jeremy D. Frens <jdfrens@gmail.com>
2007-08-23 15:54:02 +0000

    Renamed existing selenium tests to indicate clearly that they're HTML. Created test suite. Created script to run on Frens's laptop.

---
+++ test/selenium/calvin/runallujdfrens.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+firefox -chrome "chrome://selenium-ide/content/selenium/TestRunner.html?baseURL=http://cs.calvin.edu/&test=file:///home/jdfrens/Aptana/workspace/Department%20Website/test/selenium/calvin/TestSuite.html&auto=true" -height 900 -width 900
