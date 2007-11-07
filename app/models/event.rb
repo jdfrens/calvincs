cba95e9df24efc1aa1c5c012e771d263009e5458
Jeremy D. Frens <jdfrens@gmail.com>
2007-11-07 01:51:52 +0000

    Added Mocha plugin to make mocking easier. Added Behaviors plugin to use "should". Added higher-level find methods for Events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,23 @@ class Event < ActiveRecord::Base
                       range_start, range_stop,
                       range_start, range_start])
   end
+  
+  def self.find_by_week_of(date=Time.now)
+    start = date - date.wday.days
+    stop = date + 6.days - date.wday.days
+    find_within(start, stop)
+  end
+  
+  def self.find_by_semester_of(date=Time.now)
+    if date.month < 8
+      start = Time.local(date.year, 1,  1)
+      stop = Time.local(date.year,  7, 31)
+    else
+      start = Time.local(date.year, 8,  1)
+      stop = Time.local(date.year, 12, 31)
+    end
+    find_within(start, stop)
+  end
 end
 
 class Colloquium < Event
--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -2,21 +2,17 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class EventTest < Test::Unit::TestCase
   fixtures :events
-
-  # Event#find_by_week_of(date=Time.now)
-  # Event#find_by_month_of(date=Time.now)
-  # Event#find_by_semester_of(date=Time.now)
-  # all can be implemented in terms of:
-  # Event#find_within(start, stop)
   
-  def test_inheritance
+  should "have single-table inheritance" do
     assert_equal Colloquium, events(:old_colloquium).class
     assert_equal(Conference, events(:old_conference).class)
   end
 
-  def test_find_within
+  should "have a default return for finding within a range" do
     assert_equal [], Event.find_within(Time.now, 1.minute.from_now)
-    
+  end
+  
+  should "find within a range based on all possible overlappings" do
     event = events(:todays_colloquium)
     assert_equal [], Event.find_within(event.stop + 2.hours, event.stop + 3.hours)
     assert_equal [], Event.find_within(event.start - 3.hours, event.start - 2.hours)
@@ -24,8 +20,43 @@ class EventTest < Test::Unit::TestCase
     assert_equal [event], Event.find_within(event.start - 1.minute, event.stop + 1.minute)
     assert_equal [event], Event.find_within(event.start + 1.minute, event.stop - 1.minute)
     assert_equal [event], Event.find_within(event.start - 1.minute, event.stop - 1.minute)
-    
+  end
+  
+  should "find more than one event with a given range" do
     assert_equal [events(:todays_colloquium), events(:within_a_week_colloquium), events(:within_a_month_colloquium)],
       Event.find_within(Time.now, 2.months.from_now)
+  end
+  
+  should "find within a whole week" do
+    sunday =    Time.local(2007, 11,  4, 13)
+    monday =    Time.local(2007, 11,  5, 13)
+    tuesday =   Time.local(2007, 11,  6, 13)
+    wednesday = Time.local(2007, 11,  7, 13)
+    thursday =  Time.local(2007, 11,  8, 13)
+    friday =    Time.local(2007, 11,  9, 13)
+    saturday =  Time.local(2007, 11, 10, 13)
+    
+    [sunday, monday, tuesday, wednesday, thursday, friday, saturday].each do |day|
+      Event.expects(:find_within).with(sunday, saturday).returns(:the_events)
+      assert_same(:the_events, Event.find_by_week_of(day))
+    end
+  end
+  
+  should "find in fall semester" do
+    fall    = Time.local(2007, 11, 12, 13)
+    Event.expects(:find_within).with(Time.local(2007, 8,  1), Time.local(2007, 12, 31)).returns(:fall_events)
+    assert_same(:fall_events, Event.find_by_semester_of(fall))
+  end
+
+  should "find in interim (actually spring semester)" do
+    interim = Time.local(2007,  1,  8, 13)
+    Event.expects(:find_within).with(Time.local(2007, 1,  1), Time.local(2007,  7, 31)).returns(:interim_events)
+    assert_same(:interim_events, Event.find_by_semester_of(interim))
+  end
+
+  should "find in spring semester" do
+    spring  = Time.local(2007,  3,  7, 13)
+    Event.expects(:find_within).with(Time.local(2007, 1,  1), Time.local(2007,  7, 31)).returns(:spring_events)
+    assert_same(:spring_events, Event.find_by_semester_of(spring))
   end
 end
