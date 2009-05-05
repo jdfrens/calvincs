09692d91c457882fdfd70f025c4404e92f915fa2
Jeremy D. Frens <jdfrens@gmail.com>
2009-05-05 18:05:29 -0400

    Deleting old functional tests.  Keeping selenium directory until I can find it a better home.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,3 +94,34 @@ def assert_remote_form_for_and_spinner(id, route)
   form.attributes["onsubmit"] =~ /Ajax\.Request\('(.+?)'/
   assert_equal route, "#$1", "should have correct route in Ajax request"
 end
+
+class ImageInfo
+
+  def self.fake_size(url, size)
+    @@sizes ||= {}
+    @@sizes[url] = size
+  end
+
+  def initialize(url)
+    @@seen_urls ||= []
+    @@seen_urls << url
+    @url = url
+  end
+
+  def width
+    if @@sizes[@url]
+      @@sizes[@url][:width]
+    else
+      raise "#{@url} was unexpected"
+    end
+  end
+
+  def height
+    if @@sizes[@url]
+      @@sizes[@url][:height]
+    else
+      raise "#{@url} was unexpected"
+    end
+  end
+
+end
