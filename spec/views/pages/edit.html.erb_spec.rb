efdc3a92cc2c423f02c5d55e4c513848a2d9db9d
Jeremy D. Frens <jdfrens@gmail.com>
2009-05-21 08:06:36 -0400

    discovered some RSpec examples that had been commented out; about to try a routing experiment

--- spec/views/pages/edit.html.erb_spec.rb
+++ spec/views/pages/edit.html.erb_spec.rb
@@ -1,45 +1,69 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "/page/edit.html.erb" do
+describe "/pages/edit.html.erb" do
 
-#  it "should handle a wide image" do
-#    page = mock_model(Page, :subpage? => false, :title => "Mission Statement", :content => "We state our mission.")
-#    assigns[:page] = page
-#
-#    render "/page/view"
-#
-#    assert_select "div#content" do
-#      assert_select "h1", "Mission Statement"
-#      assert_select "div#page_content", /We state our mission./
-#      assert_select "div.img-right-wide" do
-#        assert_select "img#cool-pic"
-#        assert_select "p.img-caption", images(:mission_wide).caption
-#      end
-#      assert_select "h1 #page_title_1_in_place_editor", false
-#      assert_select "p #page_content_1_in_place_editor", false
-#      assert_select "p.identifier #page_identifier_1_in_place_editor", false
-#    end
-#  end
+  describe "render a normal page" do
+    before(:each) do
+      @page = mock_model(Page, :subpage? => false, :title => "Mission Statement",
+              :content => "We state our mission.", :identifier => "mission")
+      assigns[:page] = @page
+      template.should_not_receive(:render).with(:partial => "image")
+      expect_textilize("We state our mission.")
+      template.should_receive(:in_place_editor_field).with(:page, "title").and_return("title's in-place editor")
+      template.should_receive(:in_place_editor_field).with(:page, "identifier").and_return("identifier's in-place editor")
 
-#  it "should handle narrow image" do
-#    get :view, :id => 'mission_narrow'
-#
-#    assert_response :success
-#    assert_select "div#content" do
-#      assert_select "div.img-right-narrow" do
-#        assert_select "img#cool-pic"
-#        assert_select "p.img-caption", images(:mission_narrow).caption
-#      end
-#    end
-#  end
-#
-#  it "should handle no image" do
-#    get :view, :id => 'alphabet'
-#
-#    assert_response :success
-#    assigns[:title].should == pages(:alphabet).title
-#    assigns[:last_updated].should == pages(:alphabet).updated_at
-#    assert_select "div#content div.img-right", false
-#  end
+      render "/pages/edit"
+    end
 
+    it "should have an editor for the title" do
+      response.should have_selector("h1") do |h1|
+        h1.should contain("title's in-place editor")
+      end
+    end
+
+    it "should not annotate the title" do
+      response.should_not have_selector("em", :content => "rarely seen")
+    end
+
+    it "should display the textilized page content" do
+      response.should have_selector("#page_content") do |element|
+        element.should contain("We state our mission.")
+      end
+    end
+
+    it "should have an editor for the page content" do
+      response.should have_selector("form", :action => "/pages/update_page_content/#{@page.id}") do |element|
+        element.should contain("We state our mission.")
+      end
+    end
+
+    it "should have an editor for the identifier" do
+      response.should have_selector("p.identifier") do |p|
+        p.should contain("identifier's in-place editor")
+      end
+    end
+
+    it "should link back to show the page" do
+      pending()
+      response.should have_selector("a", :href => "/p/mission", :content => 'show page')
+    end
+  end
+
+  describe "render a subpage" do
+    before(:each) do
+      @page = mock_model(Page, :subpage? => true, :title => "Whatever", :content => "No, really, whatever!")
+      assigns[:page] = @page
+      template.should_not_receive(:render).with(:partial => "image")
+      template.should_receive(:in_place_editor_field).with(:page, "title").and_return("title's in-place editor")
+      template.should_receive(:in_place_editor_field).with(:page, "identifier").and_return("identifier's in-place editor")
+
+      render "/pages/edit"
+    end
+
+    it "should annotate the title" do
+      response.should have_selector("h1") do |h1|
+        h1.should have_selector("em", :content => "rarely seen")
+      end
+    end
+  end
 end
