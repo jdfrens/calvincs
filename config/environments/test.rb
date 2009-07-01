dc5d08518b3017847a34463ea773f125b98480f6
Jeremy D. Frens <jdfrens@gmail.com>
2009-07-01 14:17:53 -0400

    Upgrading some gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,7 @@ config.action_controller.perform_caching             = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem "rspec", :lib => false, :version => ">= 1.2.6"
-config.gem "rspec-rails", :lib => false, :version => ">= 1.2.6"
+config.gem "rspec", :lib => false, :version => ">= 1.2.7"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.7"
 config.gem "aslakhellesoy-cucumber", :lib => "cucumber", :version => ">= 0.3.11"
 config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com", :version => ">= 2.10.1"
