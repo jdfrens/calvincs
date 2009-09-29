f9217b6a0728970a251b6ac39483fcb89d0e47cd
Jeremy D. Frens <jdfrens@gmail.com>
2009-09-29 17:33:34 -0500

    adding rcov to the required gems

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ ab42f1f26f41a724f67ac07538d'
   config.gem "RedCloth", :lib => 'redcloth' #, :version => ">= 4.0"
   config.gem "imagesize", :lib => 'image_size'
   config.gem 'jscruggs-metric_fu', :lib => 'metric_fu', :source => 'http://gems.github.com'
+  config.gem 'rcov'
 end
 
 # time formats
