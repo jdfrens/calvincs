cf0a4656048380e42431d6c4ec0092721fb51db8
Jeremy D. Frens <jdfrens@gmail.com>
2010-03-27 13:32:23 -0500

    trying out the NewRelic RPM for performance monitoring

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :deploy_to, "/srv/www/#{application}"
 set :gateway, "jdfrens@cs-ssh.calvin.edu"
 
 after "deploy:update_code", :roles => :app, :except => {:no_symlink => true} do
-  ["config/database.yml", "config/mongrel_cluster.yml", "config/secret.txt"].each do |file|
+  ["config/database.yml", "config/mongrel_cluster.yml", "config/newrelic.yml", "config/secret.txt"].each do |file|
     run <<-CMD
       ln -nfs #{shared_path}/#{file} #{release_path}/#{file}
     CMD
