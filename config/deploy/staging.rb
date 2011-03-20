8f4560169ff77af9950a183fb2c9fb41c464fe5a
Jeremy D. Frens <jdfrens@gmail.com>
2011-03-20 17:07:51 -0500

    Tweak deployment (to staging) for Passenger 3.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,8 +5,16 @@ role :db, "yags.calvin.edu", :primary => true
 set :branch, "staging"
 
 namespace :deploy do
-  task :restart do
-    # sudo "/etc/init.d/apache2 restart"
-    run "cd #{deploy_to}/current ; #{sudo} bundle exec mongrel_rails cluster::restart -C #{mongrel_config}"
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
