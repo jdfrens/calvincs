4202f3dfbe0d9d0dc7e15d1112fe666646f51185
Jeremy D. Frens <jdfrens@gmail.com>
2011-04-10 12:45:08 -0500

    Deploys with passener in production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,20 @@
-role :web, "cerf"
-role :app, "cerf"
-role :db, "cerf", :primary => true
+role :web, "cerf.calvin.edu"
+role :app, "cerf.calvin.edu"
+role :db, "cerf.calvin.edu", :primary => true
 
-set :branch, "production"
+set :branch, "deploy"
 
 namespace :deploy do
-  task :restart do
-    sudo "mongrel_rails cluster::restart -C #{mongrel_config}"
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
